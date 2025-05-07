9638aedcaecb41acea47d53056145729ddfd68ec
Ben Slater <ben@theslaters.com.au>
2025-05-07 11:25:10 +1000

    d

--- Bodu.Core/src/Collections.Extensions/IEnumerableExtensions.CountOrDefault.cs
+++ Bodu.Core/src/Collections.Extensions/IEnumerableExtensions.CountOrDefault.cs
@@ -4,6 +4,7 @@
 // </copyright>
 // ---------------------------------------------------------------------------------------------------------------
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
